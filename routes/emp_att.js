29e0778b708d3a77966cca0aad32e4b7a76d1166
Teody Miranda <mirandateodyc23@gmail.com>
2020-10-25 19:44:00 +0800

    added skype attendance format

--- routes/emp_att.js
+++ routes/emp_att.js
@@ -1,3 +1,4 @@
+const e = require('express');
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
@@ -21,19 +22,83 @@ function jsonReader(filePath, cb) {
 /* GET home page. */
 router.get('/', function(req, res) {
   var empdata = [];
-  jsonReader('./data.json', (err, emp) => {
+  var result_msg = "";
+  
+  jsonReader('./data.json', (err, employees) => {
     if (err) {
         console.log(err)
         return
     }
-      empdata = emp;
-      res.render('emp_att', {
-        title: 'Employee Form',
-        data: empdata
-       });
+    // empdata = emp;
+    for (var i in employees.employee) {
+      var emp = employees.employee[i];
+      delete emp.emp_id_new;
+      empdata.push(emp);
+    }
+    console.log(empdata);
+    
+    res.render('emp_att', {
+      title: 'Employee Form',
+      title2: 'Attendance Format',
+      data: empdata,
+      result: result_msg
+      });
       
   });
 
+});
+
+  /* POST home page. */
+router.post('/', function(req, res) {
+  var empdata = [];
+  var result_msg = "";
+  var postdata = JSON.parse(JSON.stringify(req.body));
+  var status = 0;
+  var message = "";
+  if(postdata.emp_id !== ''){
+    status = 1
+    message = "Attendance format has successfuly generated."
+    var att_msg = [];
+    var date_obj = new Date();
+    var month = date_obj.getMonth()+ 1;
+    var day = date_obj.getUTCDate();
+    var year = date_obj.getUTCFullYear();
+    var months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+  
+    att_msg.push(months[month-1] + " " + day + ", " + year);
+    att_msg.push('EMP ID: ' + postdata.emp_id);
+    att_msg.push('Name: ' + postdata.emp_name);
+    att_msg.push('Time: ' + postdata.emp_shift);
+    att_msg.push('Remote location: ' + postdata.emp_loc);
+    result_msg = att_msg.join('<br/>');
+  } else {
+    status = 0;
+    message = "Please choose employee first."
+  }
+  jsonReader('./data.json', (err, employees) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    // empdata = emp;
+    for (var i in employees.employee) {
+      var emp = employees.employee[i];
+      delete emp.emp_id_new;
+      empdata.push(emp);
+    }
+    console.log(empdata);
+    
+    res.render('emp_att', {
+      title: 'Employee Form',
+      title2: 'Attendance Format',
+      data: empdata,
+      result: result_msg,
+      status : status,
+      message: message,
+      });
+      
+  });
+      
 });
 
 router.post('/save_emp', function(req, res) {
@@ -42,41 +107,64 @@ router.post('/save_emp', function(req, res) {
   var empdata = [];
   var postdata = JSON.parse(JSON.stringify(req.body));
 
-  if(postdata.emp_id_new.length > 0){
-    postdata.emp_id = postdata.emp_id_new;
-    delete postdata.emp_id_new;
-  }
-
-  jsonReader(filePath, (err, employees) => {
-    if (err) {
-        console.log(err)
-        return
+  if(postdata.emp_id_new == '' && postdata.emp_id == ""){
+    res.send({result: false});
+  } else {
+    if(postdata.emp_id_new.length > 0){
+      postdata.emp_id = postdata.emp_id_new;
+      delete postdata.emp_id_new;
     }
-    console.log(postdata);
-    var exist = false;
-    for (var i in employees.employee) {
-      var emp = employees.employee[i];
-      if(emp.emp_id == postdata.emp_id){
-        exist = true;
-        emp = postdata;
+  
+    jsonReader(filePath, (err, employees) => {
+      if (err) {
+          console.log(err)
+          return
       }
-      empdata.push(emp);
-
-    }
-
-    if(!exist){
-      empdata.push(postdata);
-    }
-
-    fs.writeFile(filePath, JSON.stringify({employee : empdata}), (err) => {
-      if (err) console.log('Error writing file:', err)
+      console.log(postdata);
+      var exist = false;
+      for (var i in employees.employee) {
+        var emp = employees.employee[i];
+        if(emp.emp_id == postdata.emp_id){
+          exist = true;
+          emp = postdata;
+        }
+        empdata.push(emp);
+  
+      }
+  
+      if(!exist){
+        empdata.push(postdata);
+      }
+  
+      fs.writeFile(filePath, JSON.stringify({employee : empdata}), (err) => {
+        if (err) console.log('Error writing file:', err)
+      })
+      res.send({result: empdata});
+  
     })
-    res.send({result: empdata});
-
-  })
-   
-
+  }
 });
+
+router.post('/get_att', function(req, res){
+  var att_msg = [];
+  var postdata = JSON.parse(JSON.stringify(req.body));
+  var date_obj = new Date();
+  var month = date_obj.getMonth()+ 1;
+  var day = date_obj.getUTCDate();
+  var year = date_obj.getUTCFullYear();
+  var months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+
+  att_msg.push(months[month] + " " + day + ", " + year);
+  att_msg.push('EMP ID: ' + postdata.emp_id);
+  att_msg.push('Name: ' + postdata.emp_name);
+  att_msg.push('Time: ' + postdata.emp_shift);
+  att_msg.push('Remote location: ' + postdata.emp_loc);
+
+  console.log(att_msg);
+  console.log(att_msg.join('<br/>'));
+  res.render('emp_att', {result: att_msg.join('<br/>')});
+});
+
 
 function digit2_format(number) {
   return number < 10 ? ("0" + number) : number;
